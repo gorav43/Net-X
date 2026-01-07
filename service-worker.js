b8b292d9356481507cbacf7762cccbd244437bfa
gorav43 <goravdhadhich@gmail.com>
2026-01-07 09:46:55 +0530

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "netx-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
